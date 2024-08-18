3815c2f7c5ce5b700127f300bcd919a797760c74
rsuvaidyam <rkrahul00011@gmail.com>
2024-08-18 13:34:50 +0530

    add doctype

--- hrs/hooks.py
+++ hrs/hooks.py
@@ -13,6 +13,11 @@ app_license = "mit"
 # app_include_css = "/assets/hrs/css/hrs.css"
 # app_include_js = "/assets/hrs/js/hrs.js"
 
+fixtures = [
+    'Role',
+    'Role Profile'
+]
+
 # include js, css files in header of web template
 # web_include_css = "/assets/hrs/css/hrs.css"
 # web_include_js = "/assets/hrs/js/hrs.js"
---
+++ hrs/hrs/doctype/hrs_user/test_hrs_user.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, rahul sah and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestHrsUser(FrappeTestCase):
+	pass
