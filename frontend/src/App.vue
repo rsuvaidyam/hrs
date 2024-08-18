d4679bd2f60e0a9bf073a933b2fc45c48b7f04b4
rsuvaidyam <rkrahul00011@gmail.com>
2024-08-18 15:20:10 +0530

    add tailwind

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,6 +7,7 @@
 
 
 <script>
+import './style.css';
 export default {
 	inject: ['$auth']
 };
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,23 +1,127 @@
-import path from 'path';
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import proxyOptions from './proxyOptions';
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import { VitePWA } from "vite-plugin-pwa"
+import frappeui from "frappe-ui/vite"
 
-// https://vitejs.dev/config/
+import path from "path"
+import fs from "fs"
+
 export default defineConfig({
-	plugins: [vue()],
 	server: {
 		port: 8080,
-		proxy: proxyOptions
+		proxy: getProxyOptions(),
 	},
+	plugins: [
+		vue(),
+		frappeui(),
+		VitePWA({
+			registerType: "autoUpdate",
+			strategies: "injectManifest",
+			injectRegister: null,
+			devOptions: {
+				enabled: true,
+			},
+			manifest: {
+				display: "standalone",
+				name: "Hrs",
+				short_name: "Hrs",
+				start_url: "/hrs",
+				description: "Everyday HRS",
+				theme_color: "#ffffff",
+				icons: [
+					{
+						src: "/assets/hrs/manifest/manifest-icon-192.maskable.png",
+						sizes: "192x192",
+						type: "image/png",
+						purpose: "any",
+					},
+					{
+						src: "/assets/hrs/manifest/manifest-icon-192.maskable.png",
+						sizes: "192x192",
+						type: "image/png",
+						purpose: "maskable",
+					},
+					{
+						src: "/assets/hrs/manifest/manifest-icon-512.maskable.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "any",
+					},
+					{
+						src: "/assets/hrs/manifest/manifest-icon-512.maskable.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "maskable",
+					},
+				],
+			},
+		}),
+	],
 	resolve: {
 		alias: {
-			'@': path.resolve(__dirname, 'src')
-		}
+			"@": path.resolve(__dirname, "src"),
+		},
 	},
 	build: {
-		outDir: '../hrs/public/frontend',
+		outDir: "../hrs/public/frontend",
 		emptyOutDir: true,
-		target: 'es2015',
+		target: "es2015",
+		commonjsOptions: {
+			include: [/tailwind.config.js/, /node_modules/],
+		},
+		sourcemap: true,
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					"frappe-ui": ["frappe-ui"],
+				},
+			},
+		},
 	},
-});
+	optimizeDeps: {
+		include: [
+			"frappe-ui > feather-icons",
+			"showdown",
+			"tailwind.config.js",
+			"engine.io-client",
+		],
+	},
+})
+
+function getProxyOptions() {
+	const config = getCommonSiteConfig()
+	const webserver_port = config ? config.webserver_port : 8000
+	if (!config) {
+		console.log("No common_site_config.json found, using default port 8000")
+	}
+	return {
+		"^/(app|login|api|assets|files|private)": {
+			target: `http://127.0.0.1:${webserver_port}`,
+			ws: true,
+			router: function (req) {
+				const site_name = req.headers.host.split(":")[0]
+				console.log(`Proxying ${req.url} to ${site_name}:${webserver_port}`)
+				return `http://${site_name}:${webserver_port}`
+			},
+		},
+	}
+}
+
+function getCommonSiteConfig() {
+	let currentDir = path.resolve(".")
+	// traverse up till we find frappe-bench with sites directory
+	while (currentDir !== "/") {
+		if (
+			fs.existsSync(path.join(currentDir, "sites")) &&
+			fs.existsSync(path.join(currentDir, "apps"))
+		) {
+			let configPath = path.join(currentDir, "sites", "common_site_config.json")
+			if (fs.existsSync(configPath)) {
+				return JSON.parse(fs.readFileSync(configPath))
+			}
+			return null
+		}
+		currentDir = path.resolve(currentDir, "..")
+	}
+	return null
+}
