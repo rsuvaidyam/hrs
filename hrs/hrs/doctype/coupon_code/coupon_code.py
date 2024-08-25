4affb68a5890e6d86ed5fd6a580bd90865088d08
bittu9835 <bk2671111@gmail.com>
2024-08-25 11:19:42 +0530

    add

--- hrs/hrs/doctype/coupon_code/coupon_code.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, rahul sah and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class CouponCode(Document):
-	pass
