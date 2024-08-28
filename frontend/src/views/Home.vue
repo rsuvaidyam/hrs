4658ab5d42699c273143b3aea14212c478469e93
rsuvaidyam <rkrahul00011@gmail.com>
2024-08-28 11:56:45 +0530

    change ui for product

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,16 +1,16 @@
 <template>
 	<Carousel />
 	<Category />
-	<div class="px-3 md:px-8 pt-6 max-w-[1800px] mx-auto w-full">
+	<!-- <div class="px-3 md:px-8 pt-6 max-w-[1800px] mx-auto w-full">
 		<Event />
-	</div>
+	</div> -->
 	<ProductGrByEvent />
 </template>
 
 <script setup>
 import Carousel from '../components/Carousel.vue';
 import Category from '../components/Category.vue';
-import Event from '../components/Event.vue';
+// import Event from '../components/Event.vue';
 import ProductGrByEvent from '../components/product/ProductGrByEvent.vue';
 
 </script>
