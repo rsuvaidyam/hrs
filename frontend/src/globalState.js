d50a30f6a1060cfb24e01918e8b1a4118ca37488
bittu9835 <bk2671111@gmail.com>
2024-08-29 12:38:18 +0530

    add geolocation

--- frontend/src/globalState.js
+++ frontend/src/globalState.js
@@ -2,7 +2,9 @@ import { reactive, ref } from 'vue';
 
 const location_ditecter = ref(localStorage.getItem('location_ditecter'));
 const openpop = ref(false);
+const address = ref('');
 export const store = reactive({
     location_ditecter:location_ditecter.value,
-    openpop:openpop.value
+    openpop:openpop.value,
+    address:address.value,
 });
